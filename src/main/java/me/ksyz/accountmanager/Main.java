f77ba6c52417e13f5756d3d36f7bf1f1737ae7cb
ksyz <ksyz@duck.com>
2022-05-29 12:30:00 +0200

    Update Gradle files

--- src/main/java/me/ksyz/accountmanager/Main.java
+++ src/main/java/me/ksyz/accountmanager/Main.java
@@ -5,10 +5,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = Main.MODID, version = Main.VERSION)
+@Mod(
+  modid = Main.MODID, version = Main.VERSION,
+  clientSideOnly = true, acceptedMinecraftVersions = "1.8.9"
+)
 public class Main {
   public static final String MODID = "accountmanager";
-  public static final String VERSION = "1.0";
+  public static final String VERSION = "@VERSION@";
 
   @EventHandler
   public static void init(final FMLInitializationEvent event) {
